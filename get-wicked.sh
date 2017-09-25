c864090e6b80b43289e437395afc85aba682127a
Martin Danielsson <martin@danielsson.de>
2017-09-25 13:58:59 +0200

    Test script for downloading the latest chart

---
+++ get-wicked.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+latestVersion=$(curl https://api.github.com/repos/Haufe-Lexware/wicked.haufe.io/releases/latest | grep tag_name | cut -d '"' -f 4 | tr -d v)
+
+echo "INFO: Latest release is v${latestVersion}."
+echo "INFO: Downloading Helm Chart..."
+
+helm fetch --untar https://github.com/Haufe-Lexware/wicked.haufe.io/releases/download/v${latestVersion}/wicked-${latestVersion}.tgz
+
+echo "INFO: Helm Chart has been downloaded into directory 'wicked'."
