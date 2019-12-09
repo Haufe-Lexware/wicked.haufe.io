5efd8c3bbb33913cc8f24ae30d167685d09bff29
Danielsson <martin.danielsson@haufe-lexware.com>
2019-12-09 14:15:27 +0100

    Bump to version 1.0.0-rc.11, fix set version script

--- src/tools/release/set-version.sh
+++ src/tools/release/set-version.sh
@@ -15,40 +15,28 @@ fi
 
 subDirList="${versionDirs}" # from _repos.sh
 
-echo "Checking current branches..."
 allOkay=true
-for d in ${subDirList}; do
-    pushd $d > /dev/null
-    currentBranch=$(git branch | grep \* | cut -d ' ' -f2)
-    if [[ "${currentBranch}" != "next" ]]; then
-        echo "Repository ${d}: Current branch is '${currentBranch}', MUST be 'next'."
-        allOkay=false
-    fi
-    popd > /dev/null
-done
+echo "Checking branch..."
+currentBranch=$(git branch | grep \* | cut -d ' ' -f2)
+if [[ "${currentBranch}" != "next" ]]; then
+    echo "ERROR: Current branch is '${currentBranch}', MUST be 'next'."
+    allOkay=false
+fi
 
 if [[ $allOkay != true ]]; then
-    echo "Exiting due to errors."
+    echo "ERROR: Exiting due to errors."
     exit 1
 fi
 
-echo "Branches are all set to 'next', updating package.json."
+echo "=============================================="
+echo "Branch is set to 'next', updating package.json files."
+echo "=============================================="
 
 export WICKED_DIRS="${versionDirs}"
-node wicked.tools/release/set-version $1
+node tools/release/set-version $1
 
-for d in ${subDirList}; do
-    pushd $d > /dev/null
-    gitStatus=$(git status -s package.json)
-    if [[ "${gitStatus}" == "" ]]; then
-        echo "INFO: package.json in $d is unchanged, not pushing."
-    else
-        git add package.json
-        git commit -m "Bump to version $1"
-        git push
-        echo ""
-    fi
-    popd > /dev/null
-done
+echo "=============================================="
+echo "Done."
+echo "=============================================="
 
 popd
