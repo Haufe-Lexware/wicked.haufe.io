d17bd07902206c33909833cf6aaa669b89bfb6c0
Danielsson <martin.danielsson@haufe-lexware.com>
2019-12-16 11:01:52 +0100

    Fixes #238 - If KONG_PG_HOST is set, do not check KONG_DATABASE_SERVICE_HOST - This maintains backwards compatibility, while still fixing the bug

--- src/kong/startup.sh
+++ src/kong/startup.sh
@@ -1,25 +1,30 @@
 #!/usr/local/bin/dumb-init /bin/bash
 
 if [ ! -z "$PROXY_SSL_CERT" ] && [ ! -z "$PROXY_SSL_KEY" ]; then
-  echo Using certificates from PROXY_SSL_CERT and PROXY_SSL_KEY
+  echo "INFO: Using certificates from PROXY_SSL_CERT and PROXY_SSL_KEY"
 
   echo "$PROXY_SSL_CERT" > /root/proxy-cert.pem
   echo "$PROXY_SSL_KEY" > /root/proxy-key.pem
 
   cp /root/nginx_kong.lua /usr/local/share/lua/5.1/kong/templates/nginx_kong.lua
 else
-  echo Not using custom proxy SSL certificate. Override
-  echo by defining PROXY_SSL_CERT and PROXY_SSL_KEY.
+  echo "INFO: Not using custom proxy SSL certificate. Override"
+  echo "INFO: by defining PROXY_SSL_CERT and PROXY_SSL_KEY."
 fi
 
-if [ ! -z "$KONG_DATABASE_SERVICE_HOST" ]; then
-  echo Kubernetes Mode, picking up database host from env var.
-  unset KONG_PG_HOST
-  export KONG_PG_HOST=$KONG_DATABASE_SERVICE_HOST
+if [ -z "$KONG_PG_HOST" ]; then
+  echo "WARN: Env var KONG_PG_HOST is not set; checking KONG_DATABASE_SERVICE_HOST."
+  if [ -z "$KONG_DATABASE_SERVICE_HOST" ]; then
+    echo "ERROR: Neither KONG_PG_HOST nor KONG_DATABASE_SERVICE_HOST is set!"
+    exit 1
+  else
+    echo "INFO: Kubernetes Mode, picking up database host from env var."
+    export KONG_PG_HOST=$KONG_DATABASE_SERVICE_HOST
+  fi
 fi
 
-echo Using kong database host: $KONG_PG_HOST
-echo Trusting all IPs to send correct X-Forwarded-Proto values
+echo "Using kong database host: $KONG_PG_HOST"
+echo "Trusting all IPs to send correct X-Forwarded-Proto values"
 export KONG_TRUSTED_IPS="0.0.0.0/0,::/0"
 export KONG_ADMIN_LISTEN="0.0.0.0:8001"
 
