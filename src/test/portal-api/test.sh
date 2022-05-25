72477a356e9e9ecf1039088ae9b2b92a5aa965fe
Martin Danielsson <martin@danielsson.de>
2022-05-25 09:51:18 +0200

    Debugging messages

--- src/test/portal-api/test.sh
+++ src/test/portal-api/test.sh
@@ -5,11 +5,26 @@ echo "Mocha version: $(mocha --version)"
 
 node node_modules/portal-env/await.js http://portal-api:3001/ping
 
-mkdir test_results
-mocha > test_results/api-test.log || echo Integration tests failed. See log. > test_results/API_FAILED 
+echo ""
+echo "Directory ${pwd}"
+echo "==================="
+echo ""
 
-echo Trying to kill portal-api...
+ls -la
+
+echo ""
+echo "node_modules"
+echo "============"
+echo ""
+
+
+ls -la node_modules
+
+mkdir test_results
+mocha > test_results/api-test.log || echo "Integration tests failed. See log. > test_results/API_FAILED"
+
+echo "Trying to kill portal-api..."
 
 curl -X POST http://portal-api:3001/kill
 
-echo Exiting.
+echo "Exiting."
